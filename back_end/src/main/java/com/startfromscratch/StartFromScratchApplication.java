bfa31672510107b88702229ae3db676871024dd4
idealCounty <1299351533@qq.com>
2025-05-01 11:55:15 +0800

    init

---
+++ back_end/src/main/java/com/startfromscratch/StartFromScratchApplication.java
@@ -0,0 +1,13 @@
+package com.startfromscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartFromScratchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartFromScratchApplication.class, args);
+    }
+
+}
---
+++ back_end/src/test/java/com/startfromscratch/StartFromScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.startfromscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartFromScratchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
