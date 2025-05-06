598746b7956567f992d6466d2e816f5694b12d59
FlyAwake <3085384553@qq.com>
2025-05-06 21:14:45 +0800

    router

--- src/views/user/Login.vue
+++ src/views/user/Login.vue
@@ -26,7 +26,7 @@ function handleLogin() {
     if (res.data.code === '000') {
       const token = res.data.result
       sessionStorage.setItem('token', token)
-      router.push({path: "/init"})
+      router.push({path: "/choose"})
     } else if (res.data.code === '400') {
       errMsg.value = '登陆失败'
       password.value = ''
